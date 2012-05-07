4c1fdbd1b6dd31c698d181029d2edbb0c78145a3
Vera Surkova <vera@surkova.se>
2012-05-07 12:36:01 +0200

    Tabbarna är borta, sökform tillagd, sökresultatvy

--- ui/js/custom.js
+++ ui/js/custom.js
@@ -3,11 +3,22 @@ $(function () {
 	$("#tab2").tab('show');
 });
 
-function initMyMap() {
-        
-              // get reference to the DIV that will hold the map.
-              var mymapDiv = document.getElementById('mymap');
+window.onload = function () {
+	initMyMap();
+}
 
-              // start an eniro map in the DIV.
-              var map = new eniro.maps.Map(mymapDiv);              
-          }
+function initMyMap() {
+	// get reference to the DIV that will hold the map.
+	var mymapDiv = document.getElementById('mymap');
+
+	// start an eniro map in the DIV.
+	var map = new eniro.maps.Map(mymapDiv, {
+		myLocationControl: true,
+	});
+
+	var mark = new eniro.maps.Marker({
+		position: new eniro.maps.LatLng(59.31269, 18.07081), 
+		icon: 'http://images2.wikia.nocookie.net/__cb20120322200704/angrybirds/images/b/bb/AB_red-bird_space.png',
+		map: mymap  // by providing map the marker is directly added
+		});
+}
