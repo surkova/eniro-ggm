649f06ac67708b38c5556b0500967adbd7f88345
Vera Surkova <vera@surkova.se>
2012-05-10 17:34:20 +0200

    Sökfältet bundet till sökresultat

--- ui/js/search.js
+++ ui/js/search.js
@@ -1,16 +1,22 @@
-$.getJSON("http://api.eniro.com/cs/search/basic",
-	{
-		profile: "ggmsearch",
-		key: "7219929822480078432",
-		country: "se",
-		version: "1.1.3",
-		search_word: "advokat"
-	},
-	function(data) {
-		$.each(data.adverts, function (key, val) {
-			$("#searchResults").append("<p><a href='"+val.companyReviews+"'>" + val.companyInfo.companyName + "</a></p>");
-			$("#searchResults").append("<p>" + val.companyInfo.companyText + "</p>");
-		});
-		//$("#searchResults").append("<p>" + data.adverts[0].companyInfo.companyName + "</p>");
-	}
-);
+$(function () {
+	$("#sok").click(function () {
+		var req = $("#req").val();
+		$.getJSON("http://api.eniro.com/cs/search/basic",
+			{
+				profile: "ggmsearch",
+				key: "7219929822480078432",
+				country: "se",
+				version: "1.1.3",
+				search_word: req
+			},
+			function(data) {
+				$("#searchResults").empty();
+				$.each(data.adverts, function (key, val) {
+					$("#searchResults").append("<p><a href='"+val.companyReviews+"'>" + val.companyInfo.companyName + "</a></p>");
+					$("#searchResults").append("<p>" + val.companyInfo.companyText + "</p>");
+				});
+			}
+		);
+		return false;
+	});
+});
