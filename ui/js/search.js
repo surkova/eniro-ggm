41ef749ae177f059cc8a1ae0b0709009925eb63d
Vera Surkova <vera@surkova.se>
2012-05-12 18:47:26 +0200

    Karta (inga markörer)

--- ui/js/search.js
+++ ui/js/search.js
@@ -1,22 +1,54 @@
 $(function () {
 	$("#sok").click(function () {
+
+		//take the content of the input field
 		var req = $("#req").val();
+		var sokarea = $("#sokarea").val();
+
+		//pass req, get json response from Eniro
+		//display company name with rejta url and company address for each hit
+		//return false not to refresh the page
 		$.getJSON("http://api.eniro.com/cs/search/basic",
 			{
 				profile: "ggmsearch",
 				key: "7219929822480078432",
 				country: "se",
 				version: "1.1.3",
-				search_word: req
+				from_list: 1,
+				to_list: 10,
+				search_word: req,
+				geo_area: sokarea
 			},
 			function(data) {
 				$("#searchResults").empty();
 				$.each(data.adverts, function (key, val) {
-					$("#searchResults").append("<p><a href='"+val.companyReviews+"'>" + val.companyInfo.companyName + "</a></p>");
-					$("#searchResults").append("<p>" + val.companyInfo.companyText + "</p>");
+					if (val.companyReviews != null && val.companyInfo.companyName != null)
+						{
+							$("#searchResults").append("<h6><a href='" + val.companyReviews+"'>" + val.companyInfo.companyName + "</a></h6>");
+							if (val.address.streetName != null)
+								{
+									$("#searchResults").append("<p>" + val.address.streetName + "</p>");
+								};
+							if (val.address.postCode != null && val.address.postArea != null)
+								{
+									$("#searchResults").append("<p>" + val.address.postCode + "&nbsp;" + val.address.postArea + "</p>");
+								};
+						};
 				});
 			}
 		);
 		return false;
 	});
+
+	//initialize the map
+	initKarta();
 });
+
+//display the map
+function initKarta() {
+	var kartaDiv = document.getElementById('karta');
+
+	var karta = new eniro.maps.Map(kartaDiv, {
+		zoom: 2
+	});
+};
