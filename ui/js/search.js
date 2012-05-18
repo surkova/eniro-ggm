636a3a61f503fe7de4e8b37e3d6ab9f81babcca6
Vera Surkova <vera@surkova.se>
2012-05-18 22:10:17 +0200

    Proximity search

--- ui/js/search.js
+++ ui/js/search.js
@@ -1,54 +1,54 @@
 $(function () {
+	var coord;
+	//initialize and manipulate the map
+	function initMap() {
+		var mapDiv = document.getElementById('map');
+
+		var map = new eniro.maps.Map(mapDiv, {
+			myLocationControl: true,
+			zoom: 2
+		});
+
+		eniro.maps.event.addListener(map, 'center_changed', function () {
+			coord = map.getCenter();
+			$("#delivery-to").val(coord.x + ", " + coord.y);
+		} );
+	};
+	initMap();
+
 	$("#sok").click(function () {
 
 		//take the content of the input field
 		var req = $("#req").val();
-		var sokarea = $("#sokarea").val();
+		var searcharea = $("#searcharea").val();
 
 		//pass req, get json response from Eniro
 		//display company name with rejta url and company address for each hit
 		//return false not to refresh the page
-		$.getJSON("http://api.eniro.com/cs/search/basic",
-			{
-				profile: "ggmsearch",
-				key: "7219929822480078432",
+		$.getJSON("http://api.eniro.com/cs/proximity/basic", {
+				profile: "ggmprox",
+				key: "3263099275612453667",
 				country: "se",
 				version: "1.1.3",
-				from_list: 1,
-				to_list: 10,
 				search_word: req,
-				geo_area: sokarea
+				latitude: coord.y,
+				longitude: coord.x
 			},
 			function(data) {
 				$("#searchResults").empty();
 				$.each(data.adverts, function (key, val) {
-					if (val.companyReviews != null && val.companyInfo.companyName != null)
-						{
-							$("#searchResults").append("<h6><a href='" + val.companyReviews+"'>" + val.companyInfo.companyName + "</a></h6>");
-							if (val.address.streetName != null)
-								{
-									$("#searchResults").append("<p>" + val.address.streetName + "</p>");
-								};
-							if (val.address.postCode != null && val.address.postArea != null)
-								{
-									$("#searchResults").append("<p>" + val.address.postCode + "&nbsp;" + val.address.postArea + "</p>");
-								};
-						};
+					if (val.companyReviews !== null && val.companyInfo.companyName !== null) {
+						$("#searchResults").append("<h6><a href='" + val.companyReviews+"'>" + val.companyInfo.companyName + "</a></h6>");
+						
+						if (val.address.streetName !== null)
+								$("#searchResults").append("<p>" + val.address.streetName + "</p>");
+						
+						if (val.address.postCode !== null && val.address.postArea !== null)
+								$("#searchResults").append("<p>" + val.address.postCode + "&nbsp;" + val.address.postArea + "</p>");
+					};
 				});
 			}
 		);
 		return false;
 	});
-
-	//initialize the map
-	initKarta();
 });
-
-//display the map
-function initKarta() {
-	var kartaDiv = document.getElementById('karta');
-
-	var karta = new eniro.maps.Map(kartaDiv, {
-		zoom: 2
-	});
-};
