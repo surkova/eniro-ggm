cce0a9a7bf4a36e35f68fbedcd5910c50de5d3b1
Vera Surkova <vera@surkova.se>
2012-05-22 14:29:16 +0200

    Datepicker, templates

--- ui/js/search.js
+++ ui/js/search.js
@@ -1,5 +1,5 @@
 $(function () {
-	var coord;
+	var currentLocation;
 
 	//initialize and manipulate the map
 	function initMap() {
@@ -7,43 +7,51 @@ $(function () {
 
 		var map = new eniro.maps.Map(mapDiv, {
 			myLocationControl: true,
-			zoom: 2
+			center: new eniro.maps.LatLng(59.33247, 18.11905),
+			zoom: 6
 		});
 
 		$(".e-mylocation:not(.e-mylocation-active)").click(function () {
 			eniro.maps.event.addListener(map, 'center_changed', function () {
-				coord = map.getCenter();
-				$("#delivery-to").val("Center: " + coord.y + ", " + coord.x);
+				currentLocation = map.getCenter();
 			} );
 		});
 	};
 	initMap();
 
+	//datepicker initialization
+	$("#datepicker").datepicker({dateFormat: "yy-mm-dd"});
+	var altFormat = $("#datepicker").datepicker("option", "dateFormat");
+	$("#datepicker").datepicker("option", "dateFormat", "yy-mm-dd");
+	$("#datepicker").attr("placeholder", "Idag");
+
+	//search
 	$("#sok").click(function () {
 		//take the content of the input field
 		var req = $("#req").val();
 
-		if (jQuery.isEmptyObject(coord)) {
+		if (jQuery.isEmptyObject(currentLocation)) {
 			$("#searchResults").append("<p>Var är du nånstans?</p>");
 			return false;
 		}
 
-		//pass req, get json response from Eniro
-		//display company name with rejta url and company address for each hit
-		//return false not to refresh the page
+		/*pass req and current geolocation, get json response from Eniro
+		display company name with rejta url, rejta rating, company address and phone for each hit
+		return false not to refresh the page*/
 		$.getJSON("http://api.eniro.com/cs/proximity/basic", {
 				profile: "ggmprox",
 				key: "3263099275612453667",
 				country: "se",
 				version: "1.1.3",
 				search_word: req,
-				latitude: coord.y,
-				longitude: coord.x
+				latitude: currentLocation.y,
+				longitude: currentLocation.x
 			},
 			function(proximityData) {
 				$("#searchResults").empty();
 
 				$.each(proximityData.adverts, function (key, val) {
+
 					$.getJSON("http://api.eniro.com/rs/company/basic", {
 						profile: "ggmrejta",
 						key: "4887234730381502142",
@@ -52,26 +60,19 @@ $(function () {
 						eniro_id: val.eniroId
 					},
 					function(ratingData) {
-						
-						if (val.companyReviews !== null && val.companyInfo.companyName !== null) {
-							$("#searchResults").append("<h3><a href='" + val.companyReviews+"'>" + val.companyInfo.companyName + "</a></h3>");
-							
-							if (ratingData.adverts[0].rating.numberOfReviews !== null & ratingData.adverts[0].rating.averageRating !== null)
-								$("#searchResults").append("<p>Omdömen " + ratingData.adverts[0].rating.numberOfReviews + ",&nbsp;Rating " + ratingData.adverts[0].rating.averageRating + "</p>");
-
-							if (val.address.streetName !== null)
-								$("#searchResults").append("<span>" + val.address.streetName + "</span>");
-							
-							if (val.address.postCode !== null && val.address.postArea !== null)
-								$("#searchResults").append("<span>" + val.address.postCode + "&nbsp;" + val.address.postArea + "</span>");
-
-							$.each(val.phoneNumbers, function (key, val) {
-								$("#searchResults").append("<span>" + val.type + "&nbsp;" + val.phoneNumber + "</span>");
-							});
-						};
+						var tpl = $('#searchTpl').html();
+						var searchHit = Mustache.to_html(tpl, {x: val, r: ratingData});
+						$('#searchResults').append(searchHit);
 					});
-				});
+				})
+
 			});
 		return false;
+	});
+	
+	/*button "leverera från dom" that appears in the search results. 
+	it copies delivery company's address to the form input*/
+	$(".deliveryHere").click(function () {
+		
 	});
 });
